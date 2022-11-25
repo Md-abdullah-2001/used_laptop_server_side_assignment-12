5858f807b0380beb8c6cef9b51b0fd08cc5630cc
Md-abdullah-2001 <mdabdullah.dm2001@gmail.com>
2022-11-26 01:05:43 +0600

    booking api posyed and get

--- index.js
+++ index.js
@@ -29,12 +29,51 @@ async function run() {
     const categoryCollection = client
       .db("gadgetBazar")
       .collection("categories");
+    const productCollection = client
+      .db("gadgetBazar")
+      .collection("all_products");
+    const bookingCollection = client.db("gadgetBazar").collection("bookings");
 
     // get category API
     app.get("/category", async (req, res) => {
       const query = {};
       const categories = await categoryCollection.find(query).toArray();
       res.send(categories);
+    });
+
+    app.get("/category/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { category_id: id };
+      const products = await productCollection.find(query).toArray();
+      res.send(products);
+    });
+
+    // booking posted
+
+    app.post("/booking", async (req, res) => {
+      const bookings = req.body;
+      // const name = req.body.product_name;
+      // const email = req.body.email;
+      const query = {
+        product_name: bookings.product_name,
+        email: bookings.email,
+      };
+      console.log(query);
+      const alreadyBooked = await bookingCollection.find(query).toArray();
+      if (alreadyBooked.length) {
+        const message = "You have a booking already";
+        return res.send({ acknowledged: false, message });
+      }
+      const result = await bookingCollection.insertOne(bookings);
+      res.send(result);
+    });
+
+    app.get("/booking", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await bookingCollection.find(query).toArray();
+      res.send(result);
     });
   } finally {
   }
